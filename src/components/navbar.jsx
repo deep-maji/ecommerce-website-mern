b44319c3fafa288b797415582b75cf4ba0c82555
deep-maji <majideep179@gmail.com>
2025-07-28 17:20:38 +0530

    Nav Struture

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,47 @@
-// import { logo } from "../assets/images/Logo.png";
+import Logo from "../assets/images/Logo.svg";
+import SearchIcon from "../assets/images/Search.svg";
+import Fav from "../assets/images/Favorites.svg";
+import User from "../assets/images/User.svg";
+import Cart from "../assets/images/Cart.svg";
+import "../assets/styles/navbar.css";
 
 export const Navbar = () => {
     return (
         <>
             <header>
                 <nav>
-                    <div id="logo-div" className="logo">
-                        {/* <img alt="logo" src={{logo}}></img> */}
+                    <div id="logo">
+                        <img src={Logo} alt="Logo"></img>
                     </div>
-                    <div id="searchbar-div"></div>
-                    <div id="actions"></div>
-                    <div id="user-actions"></div>
+                    <div id="searchbar">
+                        <div id="Search-icon-div">
+                            <label htmlFor="search">
+                                <img src={SearchIcon} alt="searchIcon"></img>
+                            </label>
+                        </div>
+                        <div id="search-bar-div">
+                            <form action="">
+                                <input type="text" id="search" name="search" placeholder="Search"></input>
+                            </form>
+                        </div>
+                    </div>
+                    <div id="actions">
+                        <div>Home</div>
+                        <div>About</div>
+                        <div>Contact US</div>
+                        <div>Blog</div>
+                    </div>
+                    <div id="user-actions">
+                        <div id="wish">
+                            <img src={Fav} alt="fav"></img>
+                        </div>
+                        <div id="cart">
+                            <img src={Cart} alt="cart"></img>
+                        </div>
+                        <div id="user">
+                            <img src={User} alt="user"></img>
+                        </div>
+                    </div>
                 </nav>
             </header>
         </>
