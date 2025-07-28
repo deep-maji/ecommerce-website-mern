4979ddc081aff064199d636941ca97aa3dc0e329
deep-maji <majideep179@gmail.com>
2025-07-28 16:39:43 +0530

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,16 @@
-import { logo } from "../assets/images/Logo.png";
+// import { logo } from "../assets/images/Logo.png";
 
 export const Navbar = () => {
     return (
         <>
             <header>
                 <nav>
+                    <div id="logo-div" className="logo">
+                        {/* <img alt="logo" src={{logo}}></img> */}
+                    </div>
+                    <div id="searchbar-div"></div>
+                    <div id="actions"></div>
+                    <div id="user-actions"></div>
                 </nav>
             </header>
         </>
