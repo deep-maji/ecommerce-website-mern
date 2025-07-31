1c4ff5bf2580954e565c3588cb51fc8f35713480
deep-maji <majideep179@gmail.com>
2025-07-31 13:14:17 +0530

    Add Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,10 @@ import Navbar from './components/navbar'
 import Footer from './components/footer'
 import Home from './components/Home'
 import Sign from './components/Sign'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './components/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path : "/",
-      element : <Home/>
-    },
-    {
-      path : "/sign",
-      element : <Sign/>
-    },
-    {
-      path : "/login",
-      element : <Login/>
-    }
-  ])
 
   return (
     <>
