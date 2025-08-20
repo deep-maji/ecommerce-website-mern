2c5f4610b29671468636fd4404d856028e305f79
Niladri Das <niladridas413@gmail.com>
2025-08-20 17:11:21 +0530

    Added cloudinary

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -1,12 +1,14 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "productImages"); 
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "products",   // folder name in Cloudinary
+    allowed_formats: ["jpg", "png", "jpeg"]
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname); 
-  }
 });
 
 export const upload = multer({ storage });
+
